e92aa2cc408ab32e33aad05f4f29192f9eaf56d5
travizflowerz <travizflowerz@users.noreply.github.com>
2019-03-12 18:12:28 -0400

    randDNA_generator

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -1,9 +1,14 @@
 #include <random>
 #include <iostream>
 #include <string>
+#include <main.cpp>
 using namespace std;
 int main ()
 {	
 	mt19937 eng(seed);
-	uniform_string_distribution<> 
+	uniform_string_distribution<> sequence(bases);
+	for (i = 0; i < n; i++)
+		randDNA = sequence(eng)
+	
+	
 	
